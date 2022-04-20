89106868fd98afe02ebf8bdf0d50cdb338ead109
Jakub Kozik <jakubkozik120@gmail.com>
2022-04-20 22:55:18 +0200

    Fix stopwatch buttons

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import {useEffect, useState} from 'react';
+import Container from './Container/Container';
+import Stopwatch from './components/Stopwatch/Stopwatch';
 
 function App() {
 
-  const [time, setTime] = useState(0);
-  const [isActive, setActive] = useState(false);
-
-  useEffect(() => {
-    let timer;
-
-    if(isActive) {
-      timer = (setInterval(() => {
-        setTime(prevValue => prevValue + 1)
-      }, 1))
-    } else if (!isActive) {
-      clearInterval(timer);
-    }
-
-    return () => clearInterval(timer);
-  }, [isActive]);
-
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Container>
+      <Stopwatch />
+    </Container>
   );
 }
 
